beba5f3280d234f47ba8a591da581d3f9299ca22
unknown <vennisabarfi@gmail.com>
2025-04-25 23:08:55 -0400

    setting up routes and endpoints. database url up and running and successfully connected to database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 import psycopg2
+from routes import health_bp, events_bp, subscriptions_bp, users_bp
+from database import databaseConnection
 # from dotenv import find_dotenv, load_dotenv
 import sys, os
 # from flask_migratepg import MigratePg
@@ -12,6 +14,9 @@ app = Flask(__name__)
 # enable cors
 CORS(app)
 
+
+# routes/endpoints
+app.register_blueprint(health_bp)
 
 # run in debug mode for now
 if __name__ == '__main__': 
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,4 @@
 from .events import events_bp
 from .subscriptions import subscriptions_bp
 from .users import users_bp
+from .health import health_bp
